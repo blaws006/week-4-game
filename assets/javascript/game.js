4633aaff59fba199736fe91abb20f97c46c33b8a
blaws006 <blaws006>
2017-08-27 18:15:12 -0400

    Need a Reset button

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,46 @@
 //Variables:
 //Computer's Generated number
-var computerNum = Math.floor(Math.random() * 120) + 19;
+var computerNum = Math.floor(Math.random() * 101) + 19;
 console.log(computerNum);
 //Crystal numbers
 var userTotal = '';
 counter = 0;
+win = '';
+loss = '';
 var addOne = '';
 var addTwo = '';
 var addThree = '';
 var addFour = '';
 // console.log(userTotal);
-var crystalOne = Math.floor(Math.random() * 12);
-var crystalTwo = Math.floor(Math.random() * 12);
-var crystalThree = Math.floor(Math.random() * 12);
-var crystalFour = Math.floor(Math.random() * 12);
+var crystalOne = Math.floor(Math.random() * 12 + 1);
+var crystalTwo = Math.floor(Math.random() * 12 + 1);
+var crystalThree = Math.floor(Math.random() * 12 + 1);
+var crystalFour = Math.floor(Math.random() * 12 + 1);
 //User Input/A way to store collective points
 
 //Win/Loss variable
-var win = 0;
-var loss = 0;
+var winCounter = 0;
+var lossCounter = 0;
 //-------------------------------------
 //Functions 
 //Random Generator for computer number
+$( document ).ready(function() {
+
+function gametart() {
+return computerNum;
+return crystalOne;
+return crystalTwo;
+return crystalThree; 
+return crystalFour;
+}
+
+
+
 function computerGenNum() {
+
 	$("#computer-number").html(computerNum);
 }
 computerGenNum();
-
-function winLossDisplay() {
-
-	$("#wins").html(win);
-	$("#losses").html(loss);
-};
-
-winLossDisplay();
 
 
 //Generator for Crystal points
@@ -65,10 +72,46 @@ $("#crystal-four").on("click", function(event){
 });
 
 $("#crystal-one, #crystal-two, #crystal-three, #crystal-four").on("click", function(event){
+	
 	counter += userTotal
 	$("#points").html(counter);
 
+	if (counter === computerNum) {
+		alert("You Win!");
+		 winCounter = winCounter + 1;
+		 console.log(winCounter);
+		 $("#wins").html(winCounter);
+		return winCounter;
+	}
+	else if(counter > computerNum) {
+
+		alert("You Lose!");
+		 lossCounter = lossCounter + 1;
+		 console.log(lossCounter);
+		 $("#losses").html(lossCounter);
+		return lossCounter;
+	}
+
 })
+
+$(".reset").on("click", function(){
+counter = 0;
+});
+
+});
+
+
+
+// function winLossDisplay() {
+// 	// winCounter += win;
+// 	// lossCounter += loss;
+	
+	
+
+	
+// };
+
+// winLossDisplay();
 
 //on-click event that adds up the points of crystals
 //Win/loss determinant function, switch case statement maybe
