2f7756906c82322bc9bca2d883b4f17770a2ce4e
blaws006 <blaws006>
2017-08-28 21:53:14 -0400

    DONE

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,34 +1,31 @@
 //Variables:
 //Computer's Generated number
-// var computerNum = Math.floor(Math.random() * 101) + 19;
 var computerNum = '';
 console.log(computerNum);
-//Crystal numbers
+//User Input/A way to store collective points
 var userTotal = '';
 counter = 0;
-win = '';
-loss = '';
+//Crystal numbers
 var addOne = '';
 var addTwo = '';
 var addThree = '';
 var addFour = '';
-// console.log(userTotal);
-// var crystalOne = Math.floor(Math.random() * 12 + 1);
 var crystalOne = '';
-// var crystalTwo = Math.floor(Math.random() * 12 + 1);
 var crystalTwo = '';
-// var crystalThree = Math.floor(Math.random() * 12 + 1);
 var crystalThree = '';
-// var crystalFour = Math.floor(Math.random() * 12 + 1);
 var crystalFour = '';
-//User Input/A way to store collective points
 
 //Win/Loss variable
+win = '';
+loss = '';
 var winCounter = 0;
 var lossCounter = 0;
+
+var audioElement = document.createElement("audio");
+ 	audioElement.setAttribute("src", "assets/sounds/400603__newagesoup__bright-triple-sweep-quick.wav");
 //-------------------------------------
 //Functions 
-//Random Generator for computer number
+//Starts the Game by radomly selecting the number of crystals needed to be collected and the number for the crystals (once per round)
 $( document ).ready(function() {
 
 function gameStart() {
@@ -48,6 +45,7 @@ return crystalFour;
 
 gameStart();
 
+//Displays number of crystals needed
 function computerGenNum() {
 $("#computer-number").html(computerNum);
 }
@@ -55,33 +53,41 @@ computerGenNum();
 
 
 
-//Generator for Crystal points
+//Generator for Crystal points - Stores numbers of crystals collected per click
 
 $("#crystal-one").on("click", function(event){
+	audioElement.play();
 	 userTotal = crystalOne;
 	console.log(userTotal);
 	return userTotal;
 });
 
 $("#crystal-two").on("click", function(event){
-	 // addTwo = crystalTwo;
+	 audioElement.play();
 	 userTotal = crystalTwo;
 	console.log(userTotal);
 	return userTotal;
 });
 
 $("#crystal-three").on("click", function(event){
+	audioElement.play();
 	 userTotal = crystalThree;
 	console.log(userTotal);
 	return userTotal;
 });
 
 $("#crystal-four").on("click", function(event){
+	audioElement.play();
 	 userTotal = crystalFour;
 	console.log(userTotal);
 	return userTotal;
 });
 
+// Adds total number of crystals collected and displays on the screen
+
+// Also has the determinant of wins/losses
+
+// Also has the win/loss counter logic
 $("#crystal-one, #crystal-two, #crystal-three, #crystal-four").on("click", function(event){
 	
 	counter += userTotal
@@ -107,6 +113,8 @@ $("#crystal-one, #crystal-two, #crystal-three, #crystal-four").on("click", funct
 
 })
 
+//Resets the game
+//Generates new numbers - Crystals needed and Crystal worth
 $(".reset").on("click", function(){
 gameStart();
 computerGenNum();
